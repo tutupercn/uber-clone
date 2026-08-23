a71829b60ac99c197d8392ddfc96a3a5cd810e9c
tutupercn <101070427+tutupercn@users.noreply.github.com>
2026-08-23 21:16:50 +0300

    feat: add free OpenStreetMap foundation

---
+++ uber_users_app/lib/widgets/free_map_view.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class FreeMapView extends StatelessWidget {
+  const FreeMapView({
+    super.key,
+    this.latitude = 40.8269,
+    this.longitude = 29.3747,
+    this.zoom = 13,
+    this.showLocationMarker = true,
+  });
+
+  final double latitude;
+  final double longitude;
+  final double zoom;
+  final bool showLocationMarker;
+
+  static final Uri _copyrightUrl =
+      Uri.parse('https://www.openstreetmap.org/copyright');
+
+  @override
+  Widget build(BuildContext context) {
+    final center = LatLng(latitude, longitude);
+
+    return FlutterMap(
+      options: MapOptions(initialCenter: center, initialZoom: zoom),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.kocaelitag.yolcu',
+          maxZoom: 19,
+        ),
+        if (showLocationMarker)
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: center,
+                width: 48,
+                height: 48,
+                child: const Icon(
+                  Icons.location_pin,
+                  size: 46,
+                  color: Color(0xFF00A884),
+                ),
+              ),
+            ],
+          ),
+        Positioned(
+          right: 6,
+          bottom: 6,
+          child: Material(
+            color: Colors.white.withValues(alpha: 0.88),
+            borderRadius: BorderRadius.circular(4),
+            child: InkWell(
+              onTap: () => launchUrl(_copyrightUrl),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 6, vertical: 3),
+                child: Text(
+                  '© OpenStreetMap katkıda bulunanlar',
+                  style: TextStyle(fontSize: 10, color: Colors.black87),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
