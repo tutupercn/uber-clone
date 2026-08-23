6ab7e289186f3b8382cde3add01f8faceac9edf0
tutupercn <101070427+tutupercn@users.noreply.github.com>
2026-08-23 21:50:20 +0300

    Migrate rider routing and map to OpenStreetMap

--- uber_drivers_app/lib/widgets/free_map_view.dart
+++ uber_drivers_app/lib/widgets/free_map_view.dart
@@ -3,6 +3,15 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+class MapCoordinate {
+  const MapCoordinate(this.latitude, this.longitude);
+
+  final double latitude;
+  final double longitude;
+
+  LatLng toLatLng() => LatLng(latitude, longitude);
+}
+
 class FreeMapView extends StatelessWidget {
   const FreeMapView({
     super.key,
@@ -10,12 +19,18 @@ class FreeMapView extends StatelessWidget {
     this.longitude = 29.3747,
     this.zoom = 13,
     this.showLocationMarker = true,
+    this.pickup,
+    this.dropoff,
+    this.route = const [],
   });
 
   final double latitude;
   final double longitude;
   final double zoom;
   final bool showLocationMarker;
+  final MapCoordinate? pickup;
+  final MapCoordinate? dropoff;
+  final List<MapCoordinate> route;
 
   static final Uri _copyrightUrl =
       Uri.parse('https://www.openstreetmap.org/copyright');
@@ -48,6 +63,42 @@ class FreeMapView extends StatelessWidget {
               ),
             ],
           ),
+        if (route.isNotEmpty)
+          PolylineLayer(
+            polylines: [
+              Polyline(
+                points: route.map((point) => point.toLatLng()).toList(),
+                color: const Color(0xFF00A884),
+                strokeWidth: 5,
+              ),
+            ],
+          ),
+        MarkerLayer(
+          markers: [
+            if (pickup != null)
+              Marker(
+                point: pickup!.toLatLng(),
+                width: 42,
+                height: 42,
+                child: const Icon(
+                  Icons.trip_origin,
+                  color: Colors.green,
+                  size: 34,
+                ),
+              ),
+            if (dropoff != null)
+              Marker(
+                point: dropoff!.toLatLng(),
+                width: 42,
+                height: 42,
+                child: const Icon(
+                  Icons.location_on,
+                  color: Colors.red,
+                  size: 40,
+                ),
+              ),
+          ],
+        ),
         Positioned(
           right: 6,
           bottom: 6,
